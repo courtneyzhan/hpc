027cbd51290011f791b9e2751d67101b22fdcb94
Zhimin Zhan <zhimin@itest2.com>
2016-09-28 18:46:15 +1000

    update working

--- temperatures.php
+++ temperatures.php
@@ -1,6 +1,11 @@
 <html>
     <head>
-        <!--PROBLEM IS LINE 78 CANNOT GET THE DATA VALUE OUT.-->
+        <!--CURRENTLY FUNCTIONAL. 
+WHEN AT SCHOOL:
+				REPLACE $dbhost WITH 10.42.68.102
+				REPLACE $dbuser AND $dbpass WITH cdeod1
+				REPLACE THE SQL QUERIES FOR EACH AREA
+				THE TABLE NAME FOR AvgTemp IS AverageTemp(erature?)-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js"></script>
 
         <script src="http://code.highcharts.com/maps/highmaps.js"></script>
@@ -167,43 +172,35 @@ $(function() {
       "type": "map",
       "data": [{
         "name": "Group 3<br>Staff Room",
-                "value": <?php echo $grp3 ?>,
-
+        "value": <?php echo $grp3 ?>,
         "path": "M852,-993L992,-993,992,-827,852,-827,852,-993"
       }, {
         "name": "J08",
-                "value": <?php echo $j08 ?>,
-
+        "value": <?php echo $j08 ?>,
         "path": "M639,-998L838,-998,838,-774,639,-774,639,-998"
       }, {
         "name": "J07",
-                "value": <?php echo $j07 ?>,
-
+        "value": <?php echo $j07 ?>,
         "path": "M425,-1000L624,-1000,624,-776,425,-776,425,-1000"
       }, {
         "name": "J06",
-                "value": <?php echo $j06 ?>,
-
+        "value": <?php echo $j06 ?>,
         "path": "M213,-1002L412,-1002,412,-778,213,-778,213,-1002"
       }, {
         "name": "J05",
-                "value": <?php echo $j05 ?>,
-
+        "value": <?php echo $j05 ?>,
         "path": "M0,-905L199,-905,199,-635,0,-635,0,-905"
       }, {
         "name": "J04",
-                "value": <?php echo $j04 ?>,
-
+        "value": <?php echo $j04 ?>,
         "path": "M154,-607L364,-607,364,-383,154,-383,154,-607"
       }, {
         "name": "J03",
-                "value": <?php echo $j03 ?>,
-
+        "value": <?php echo $j03 ?>,
         "path": "M379,-607L578,-607,578,-383,379,-383,379,-607"
       }, {
         "name": "J02",
-                "value": <?php echo $j02 ?>,
-
+        "value": <?php echo $j02 ?>,
         "path": "M591,-607L790,-607,790,-383,591,-383,591,-607"
       }, {
         "name": "J01",
@@ -211,13 +208,11 @@ $(function() {
         "path": "M801,-608L1000,-608,1000,-385,801,-385,801,-608"
       }, {
         "name": "Group 1 & 2 Staff Room",
-                "value": <?php echo $grp12 ?>,
-
+        "value": <?php echo $grp12 ?>,
         "path": "M218,-755L713,-755,713,-640,218,-640,218,-755"
       }, {
         "name": "Head of House Offices",
-                "value": <?php echo $hoh ?>,
-
+        "value": <?php echo $hoh ?>,
         "path": "M732,-753L985,-753,985,-646,732,-646,732,-753"
       }]
     }]
